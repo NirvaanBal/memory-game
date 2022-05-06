dd9e1729881e1e80b7678868c88400d895cef655
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-06 16:27:50 +0530

    Update scores on duplicate value encounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import shuffle from './utils/shuffle';
 
 import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
 import Card from './components/Card';
-import shuffle from './utils/shuffle';
 
 function App() {
   const [numbers, setNumbers] = useState(shuffle(10));
+  const [values, setValues] = useState([]);
+  const [score, setScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
 
-  const getRandomNumbers = () => {
-    setNumbers(shuffle(10));
+  const getRandomNumbers = (value) => {
+    if (values.includes(value)) {
+      if (values.length > topScore) setTopScore(values.length);
+      setScore(0);
+      setValues([]);
+    } else {
+      setValues([...values, value]);
+
+      setNumbers(shuffle(10));
+      setScore(values.length);
+    }
   };
 
   return (
     <>
       <Header />
-      <Scoreboard />
+      <Scoreboard best={topScore} score={score} />
       {numbers.map((number, index) => (
-        <Card key={index} value={number} handleClick={getRandomNumbers} />
+        <Card
+          key={index}
+          value={number}
+          handleClick={() => getRandomNumbers(number)}
+        />
       ))}
     </>
   );
--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,8 +1,8 @@
-function Scoreboard() {
+function Scoreboard({ score, best }) {
   return (
     <div className="scoreboard">
-      <h2>Current Score: 0</h2>
-      <h3>Best Score: 0</h3>
+      <h2>Current Score: {score}</h2>
+      <h3>Best Score: {best}</h3>
     </div>
   );
 }
